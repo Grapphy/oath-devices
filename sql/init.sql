280ad9243c72ad1c451dfe33267b32810210c90a
Grapphy <76534455+Grapphy@users.noreply.github.com>
2024-04-08 10:22:59 -0300

    feat/mysql-db: adding mysql server container (#16)
    * feat: adding button to disable mfa, logout too
    
    * feat: adding mysql server container

---
+++ sql/init.sql
@@ -0,0 +1,18 @@
+USE demo_oath;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    oath_secret VARCHAR(255) DEFAULT NULL,
+    mfa_enabled BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS backup_codes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    backup_code VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO users (name, password) VALUES ('Carlo Magno', 'TestPassword');
